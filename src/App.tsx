d9dd2125966cda0cd06b6950b9bff307fff3a4bd
Moad <moadboufass90@gmail.com>
2023-10-21 12:52:23 +0100

    Initial the Projet

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+
+function App() {
+  return <></>;
+}
+
+export default App;
