07ca9f79d55a0ee195f5148976ada57e8972a4e0
Moad <moadboufass90@gmail.com>
2023-11-01 22:09:45 +0100

    Handle Games With No Image

--- src/Components/ImgUrl.tsx
+++ src/Components/ImgUrl.tsx
@@ -1,6 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 const ImgUrl = (Url: string) => {
+  if (!Url) {
+    const NewUrl = "https://placehold.co/600x400/png?text=✌";
+    return NewUrl;
+  }
   const Target = "media/";
   const index = Url.indexOf(Target) + Target.length;
   const NewUrl = Url.slice(0, index) + "crop/600/400/" + Url.slice(index);
